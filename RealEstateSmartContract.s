e16838d3f9118e024d8f6bacfce6527aa19336f5
Tarquin <2022524@student.cct.ie>
2024-11-18 16:54:52 +0000

    Cleared Test Code
    Will initialise my smart contract code now.

--- RealEstateSmartContract.s
+++ RealEstateSmartContract.s
@@ -1,16 +1,2 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-//Solidity Test for Github
-
-contract SimpleStorage {
-    uint256 public storedNumber;
-
-    function store(uint256 _number) public {
-        storedNumber = _number;
-    }
-
-    function retrieve() public view returns (uint256) {
-        return storedNumber;
-    }
-}
