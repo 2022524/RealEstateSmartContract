370455c35c896d180415ff6d91bba6899a463bb3
Tarquin <2022524@student.cct.ie>
2024-11-18 17:06:01 +0000

    Changed File Format

---
+++ RealEstateSmartContract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract RealEstateSmartContract {
+    struct Property {
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        mapping;
+    }
+}
