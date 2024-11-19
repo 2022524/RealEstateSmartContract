c11369e74dff9fddf36ec43c1c9f41a53c8d7604
Tarquin <2022524@student.cct.ie>
2024-11-19 10:35:08 +0000

    List Property Function

--- RealEstateSmartContract.sol
+++ RealEstateSmartContract.sol
@@ -9,11 +9,29 @@ contract RealEstateSmartContract {
         address owner;
         mapping(address -> uint) tokenHolders;
     }
-    uint public propertyCount = 0;
+    uint public propertyCount = 0; // Counting property ID
     mapping(uint -> Property) public properties;
 
     event PropertyListed(uint propertyId, string name, uint price, address owner);
     event TokenPurchased(uint propertyId, address buyer, uint tokens);
     event Ownershiptransferred(uint propertyId, address previousOwner, address newOwner);
 
+}
+
+function listProperty(string memory _name, uint _price, uint _totalTokens) public{
+
+    require(_totalTokens > 0, "The total number of tokens must be more than 0");
+
+    require(_price > 0, "The price mst be more than zero");
+
+    propertyCount++;
+    Property storage newProperty = properties[propertyCount];
+    newProperty.id = propertyCount;
+    newProperty.name = _name;
+    newProperty.price = _price
+    newPropety.owner = msg.sender;
+    newProperty.totalTokens = _totalTokens;
+    newProperty.tokensAvailable = _totalTokens;
+    emit PropertyListed(propertyCount, _name, price, msg.sender);
+    //Function will create a new property listed
 }
