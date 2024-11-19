6ba7ac96d5374ed64314b71173de4328c3af9d92
Tarquin <2022524@student.cct.ie>
2024-11-19 11:56:01 +0000

    Property Function Update
    Also included modifier

--- RealEstateSmartContract.sol
+++ RealEstateSmartContract.sol
@@ -7,31 +7,35 @@ contract RealEstateSmartContract {
         string name;
         uint price; // REMINDER - Need to check payment in Wei*
         address owner;
-        mapping(address -> uint) tokenHolders;
+        mapping(address => uint) tokenHolders;
     }
+
     uint public propertyCount = 0; // Counting property ID
-    mapping(uint -> Property) public properties;
+    mapping(uint => Property) public properties;
 
     event PropertyListed(uint propertyId, string name, uint price, address owner);
     event TokenPurchased(uint propertyId, address buyer, uint tokens);
     event Ownershiptransferred(uint propertyId, address previousOwner, address newOwner);
 
-}
+    modifier onlyOwner(uint propertyId){
+        require(msg.sender == properties[propertyId].owner, "Not the property owner");
+    }
+    
+    // Function will create a new property listed
+    function listProperty(string memory _name, uint _price, uint _totalTokens) public{
 
-function listProperty(string memory _name, uint _price, uint _totalTokens) public{
+        require(_price > 0, "The price mst be more than 0");
+        require(_totalTokens > 0, "The total number of tokens must be more than 0");
 
-    require(_totalTokens > 0, "The total number of tokens must be more than 0");
+        propertyCount++;
+        Property storage newProperty = properties[propertyCount];
+        newProperty.id = propertyCount;
+        newProperty.name = _name;
+        newProperty.price = _price;
+        newProperty.owner = msg.sender;
+        newProperty.totalTokens = _totalTokens;
+        newProperty.tokensAvailable = _totalTokens;
+        emit PropertyListed(propertyCount, _name, _price, msg.sender);
+    }
 
-    require(_price > 0, "The price mst be more than zero");
-
-    propertyCount++;
-    Property storage newProperty = properties[propertyCount];
-    newProperty.id = propertyCount;
-    newProperty.name = _name;
-    newProperty.price = _price
-    newPropety.owner = msg.sender;
-    newProperty.totalTokens = _totalTokens;
-    newProperty.tokensAvailable = _totalTokens;
-    emit PropertyListed(propertyCount, _name, price, msg.sender);
-    //Function will create a new property listed
 }
