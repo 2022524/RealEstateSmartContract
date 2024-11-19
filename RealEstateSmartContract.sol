a898cbb7cea6c87405e8e354d6c46935020bf3fe
Tarquin <2022524@student.cct.ie>
2024-11-19 10:22:35 +0000

    Added propertyCount and events

--- RealEstateSmartContract.sol
+++ RealEstateSmartContract.sol
@@ -5,8 +5,15 @@ contract RealEstateSmartContract {
     struct Property {
         uint id;
         string name;
-        uint price;
+        uint price; // REMINDER - Need to check payment in Wei*
         address owner;
-        mapping;
+        mapping(address -> uint) tokenHolders;
     }
+    uint public propertyCount = 0;
+    mapping(uint -> Property) public properties;
+
+    event PropertyListed(uint propertyId, string name, uint price, address owner);
+    event TokenPurchased(uint propertyId, address buyer, uint tokens);
+    event Ownershiptransferred(uint propertyId, address previousOwner, address newOwner);
+
 }
